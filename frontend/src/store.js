952f7d88cd6e888b52c25cacbfe01eb11e858862
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2022-10-16 08:58:04 +0545

    Admin screens Implemented admin backend and frontend Implemented admin user delete

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,8 @@ import {
   userLoginReducer,
   userRegisterReducer,
   userUpdateProfileReducer,
+  userListReducer,
+  userDeleteReducer,
 } from "./reducers/userReducers";
 import {
   orderCreateReducer,
@@ -27,6 +29,8 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
