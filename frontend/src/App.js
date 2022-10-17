7a3aff640b15367703ca711b4467ce4db414ee36
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2022-10-17 08:07:20 +0545

    Admin product functionalities includes list, delete, create and update products and image upload configs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   return (
@@ -35,7 +37,12 @@ function App() {
             <Route path="/cart/:id" element={<CartScreen />} />
             <Route path="/cart/" element={<CartScreen />} />
             <Route path="/admin/userlist" element={<UserListScreen />} />
+            <Route path="/admin/Productlist" element={<ProductListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>
         </Container>
